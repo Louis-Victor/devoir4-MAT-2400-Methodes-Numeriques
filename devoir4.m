45198ebe5db8b36e5db845ee85b61cebb657e00c
Louis-Victor <lvcarfav@gmail.com>
2025-11-07 09:52:56 -0500

    pipi break

--- devoir4.m
+++ devoir4.m
@@ -1,20 +1,28 @@
+% la fonction
 f=@(x) 1/(1+x^2);
+% le domainne de la fonction
 domain = -5:0.01:5;
 
+% l'évaluation de f sur le domaine
 fedex=arrayfun(@(x) f(x),domain);
 
 
 plot(domain,fedex)
 hold on;
 
+% le nombre de points pris pour l'interpolation
 N = [2,4,8];
-nomLeg = arrayfun(@(k) num2str(k),N);
+% nom des polynomes P_n pour la légende
+%nomLeg = arrayfun(@(k) num2str(k),N);
 
+% génération des interpolations
 for n=N
-    n
+    % équart entre les points
     h=10/n;
 
+    % abscisse des points d'interpolation
     xi = [];
+    % ordonée des points d'interpolation
     fxi = [];
     for i=0:n
         xi(i+1) = -5+i*h;
@@ -26,5 +34,7 @@ for n=N
     plot(domain,utile);
     
 end
-legend("f(x)",["P_",nomLeg(1)],["P_",nomLeg(2)],["P_",nomLeg(3)])
+nomLeg = compose("P_%d",N);
+legend(["f(x)",nomLeg],"Location","best")
+%legend("f(x)",["P_",nomLeg(1)],["P_",nomLeg(2)],["P_",nomLeg(3)])
 hold off;

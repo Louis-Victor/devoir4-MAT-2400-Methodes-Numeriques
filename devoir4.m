62320cf88e8760e0d17dbc45a9490ec24eb35810
Louis-Victor <lvcarfav@gmail.com>
2025-11-07 09:43:44 -0500

    bonnes pratiques!

--- devoir4.m
+++ devoir4.m
@@ -1,10 +1,16 @@
 f=@(x) 1/(1+x^2);
 domain = -5:0.01:5;
 
-plot(domain,f(domain))
+fedex=arrayfun(@(x) f(x),domain);
+
+
+plot(domain,fedex)
 hold on;
 
-for n = [2, 4, 8]
+N = [2,4,8];
+nomLeg = arrayfun(@(k) num2str(k),N);
+
+for n=N
     n
     h=10/n;
 
@@ -20,4 +26,5 @@ for n = [2, 4, 8]
     plot(domain,utile);
     
 end
+legend("f(x)",["P_",nomLeg(1)],["P_",nomLeg(2)],["P_",nomLeg(3)])
 hold off;

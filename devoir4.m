b169a6054ac4d7347365d78aefd5deb21cc768b7
Louis-Victor <lvcarfav@gmail.com>
2025-11-07 09:21:15 -0500

    un commit ordinaire

--- devoir4.m
+++ devoir4.m
@@ -1,5 +1,23 @@
-step = 1;
-domain = -5:step:5;
-f=@(x) 1/(1+x.^2);
+f=@(x) 1/(1+x^2);
+domain = -5:0.01:5;
 
-f(-5:1:5)
+plot(domain,f(domain))
+hold on;
+
+for n = [2, 4, 8]
+    n
+    h=10/n;
+
+    xi = [];
+    fxi = [];
+    for i=0:n
+        xi(i+1) = -5+i*h;
+        fxi(i+1) = f(xi(i+1));
+    end
+
+    [a,b,c,utile] = polynome_newton(xi,fxi,domain);
+
+    plot(domain,utile);
+    
+end
+hold off;

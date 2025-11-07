c7ac0d2dc0070af3164ca954f0b47068aef2bc65
Louis-Victor <lvcarfav@gmail.com>
2025-11-07 10:17:24 -0500

    remise

--- devoir4.m
+++ devoir4.m
@@ -1,4 +1,5 @@
-isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;isOctave
+% compatibilité octave/matlab
+isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 
 % la fonction
 f=@(x) 1/(1+x^2);
@@ -14,6 +15,7 @@ hold on;
 
 % le nombre de points pris pour l'interpolation
 N = [2,4,8];
+
 
 % génération des interpolations
 for n=N
@@ -34,6 +36,11 @@ for n=N
     plot(domain,utile);
     
 end
+    
+xlabel('x');
+ylabel('y');
+title("Polynome d'interpolation de f(x)");
+    
 if isOctave == 0
 nomLeg = compose("P_%d",N);
 legend(["f(x)",nomLeg],"Location","best")
@@ -44,3 +51,48 @@ else
 end
 
 hold off;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+N = [2,10,20,30,40];
+H=[];
+Eh = [];
+
+% génération des interpolations
+for n=N
+    % équart entre les points
+    h=10/n;
+    H = [H,h];
+
+    % abscisse des points d'interpolation
+    xi = [];
+    % ordonée des points d'interpolation
+    fxi = [];
+    for i=0:n
+        xi(i+1) = -5+i*h;
+        fxi(i+1) = f(xi(i+1));
+    end
+
+    [a,b,c,utile] = polynome_newton(xi,fxi,domain);
+    Ex = abs(utile - fedex);
+    Eh = [Eh,max(Ex)];
+end
+
+semilogy(H,Eh,'-o');
+set(gca, 'XDir','reverse')
+title("Maximum de l'erreur en fonction de h");
+xlabel('h');
+ylabel("E(h)");
